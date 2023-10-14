ba7026ef5993432ef11dc66ca2ec7c3c8d482e67
Sarthak Dash <dash.sarthak.work@gmail.com>
2023-10-14 17:48:02 +0530

    Added graceful exits

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-mkdir scripts
-cd scripts
+
+## SCRIPTS SET UP
+# Create script directory
+mkdir script
+cd script || exit
 
 # Download migrate
 echo "Fetching migration script"
 MIGRATE_VER="v4.16.2"
-curl -L  https://github.com/golang-migrate/migrate/releases/download/$MIGRATE_VER/migrate.linux-arm64.tar.gz | tar xvz
+curl -L  https://github.com/golang-migrate/migrate/releases/download/$MIGRATE_VER/migrate.linux-arm64.tar.gz | tar xvz || exit
 
 # Download sqlc
 echo "Fetching sqlc"
-curl -L https://downloads.sqlc.dev/sqlc_1.22.0_linux_amd64.zip -o sqlc.zip
+curl -L https://downloads.sqlc.dev/sqlc_1.22.0_linux_amd64.zip -o sqlc.zip || exit
 unzip sqlc.zip
 
+# Cleanup
 rm sqlc.zip LICENSE README.md
-
+cd ..
